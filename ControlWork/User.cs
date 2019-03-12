2049463c676a31212ec6a131f0536946efb791d9
Владислав Горохов <gbg.0101@mail.ru>
2019-03-12 18:27:43 +0600

    First

---
+++ ControlWork/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlWork
+{
+    [Serializable]
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+    }
+}
